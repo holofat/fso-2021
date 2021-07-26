5f07955d69a386ae97bcb15d0e8c49f88d757169
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-07-26 22:05:10 +0700

    Connecting project to mongodb

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,8 +1,8 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -13,4 +13,33 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 
+const mongoUrl = process.env.MONGODB_URL
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => console.log("MongoDB is connect"))
+  .catch(err => console.log(err.message))
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (req, res) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      res.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (req, res) => {
+  const blog = new Blog(req.body)
+
+  blog
+    .save()
+    .then(result => {
+      res.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
