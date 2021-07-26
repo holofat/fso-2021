d61dc500be0deefb16db5c35422f5cb73e0ba1ba
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-07-26 22:53:23 +0700

    Create controller

---
+++ part4/blog/controllers/blogs.js
@@ -0,0 +1,22 @@
+const Blog = require('../models/blog')
+const router = require('express').Router()
+
+router.get('/api/blogs', (req, res) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      res.json(blogs)
+    })
+})
+
+router.post('/api/blogs', (req, res) => {
+  const blog = new Blog(req.body)
+
+  blog
+    .save()
+    .then(result => {
+      res.status(201).json(result)
+    })
+})
+
+module.exports = router
