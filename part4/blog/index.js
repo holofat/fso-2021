51d998c6041ccde8b7a574bad4db6eaa743de9a9
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-07-26 22:53:41 +0700

    Update index.js

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,45 +1,10 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URL
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => console.log("MongoDB is connect"))
-  .catch(err => console.log(err.message))
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog
-    .save()
-    .then(result => {
-      res.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server is running on port ${config.PORT}`)
 })
