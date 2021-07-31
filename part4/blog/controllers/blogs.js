3c685aa2fe6ac0f8f983089d69921da64146f549
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-07-31 09:42:50 +0700

    Update blogs.js
    adding put and delete methods, then changing to async/await

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,22 +1,48 @@
 const Blog = require('../models/blog')
 const router = require('express').Router()
 
-router.get('/api/blogs', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs)
-    })
+router.get('/', async (req, res) => {
+  try {
+    const blogs = await Blog.find({})
+    res.json(blogs)
+  } catch (e) {
+    res.json(e)
+  }
 })
 
-router.post('/api/blogs', (req, res) => {
+router.post('/', async (req, res) => {
   const blog = new Blog(req.body)
 
-  blog
-    .save()
-    .then(result => {
-      res.status(201).json(result)
-    })
+  try {
+    const savedBlog = await blog.save()
+    res.json(savedBlog)
+  } catch (e) {
+    res.status(400).json(e)
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  const body = req.body
+
+  const blogObject = {
+    likes: body.likes
+  }
+
+  try {
+    await Blog.findByIdAndUpdate(req.params.id, blogObject)
+    res.status(200).json('Success updating').end()
+  } catch (e) {
+    res.json(e)
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id)
+    res.status(204).end()
+  } catch (e) {
+    res.json(e)
+  }
 })
 
 module.exports = router
