77c581f5ec41ba6d156cf73639e3eabca5b06078
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-07-31 09:43:41 +0700

    Update blog.test.js

--- part4/blog/test/blog.test.js
+++ part4/blog/test/blog.test.js
@@ -1,80 +1,109 @@
-const listHelper = require('../utils/list_helper')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('../utils/list_helper')
+const api = supertest(app)
+const Blog = require('../models/blog')
 
-const blogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  for (let i = 0; i < helper.initialBlog.length; i++) {
+    const blogObject = new Blog(helper.initialBlog[i])
+    await blogObject.save()
   }
-]
+})
 
-describe('total likes', () => {
-  test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(blogs)
-    expect(result).toBe(36)
+describe('when there is initially some blogs saved', () => {
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlog.length)
+  })
+
+  test('the unique identifier property of blog is id', async () => {
+    const response = await api.get('/api/blogs')
+    const firstBlog = response.body[0]
+    expect(firstBlog.id).toBeDefined()
   })
 })
 
-describe('favorite blog', () => {
-  test('of the most likes by any blogs', () => {
-    const result = listHelper.favoriteBlog(blogs)
-    expect(result.likes).toEqual(12)
+describe('addition of a new note', () => {
+  test('a blog is succesfully created', async () => {
+    const blogObject = {
+      title: 'Test Blog',
+      author: 'Test Author',
+      url: 'nothing',
+      likes: 4
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogObject)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDB()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlog.length + 1)
+  })
+
+  test('blogs without likes property, will be given default value of 0', async () => {
+    const blogObject = {
+      title: 'Test Blog',
+      author: 'Test Author',
+      url: 'nothing'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogObject)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('blogs without title and url, will result 400 Bad Request', async () => {
+    const blogObject = {
+      author: 'tese'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogObject)
+      .expect(400)
   })
 })
 
-describe('most blogs', () => {
-  test('of the most blogs by any author', () => {
-    const result = listHelper.mostBlog(blogs)
-    expect(result.author).toBe('Robert C. Martin')
+describe('deletion of a blog', () => {
+  test('success with status 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDB()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDB()
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlog.length - 1)
+
+    const contents = blogsAtEnd.map(blog => blog.id)
+
+    expect(contents).not.toContain(blogToDelete.id)
   })
 })
 
-describe('most likes', () => {
-  test('of the most likes by any author', () => {
-    const result = listHelper.mostLikes(blogs)
-    expect(result.author).toBe('Edsger W. Dijkstra')
+describe('updating a blog', () => {
+  test('success with status 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDB()
+    const blogToUpdate = blogsAtStart[0]
+    const updateLikes = {
+      likes: 123
+    }
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updateLikes)
+      .expect(200)
   })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
 })
