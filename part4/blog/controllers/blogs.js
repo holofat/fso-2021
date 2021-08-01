ada81c27c73836ba8d70fc6c4f6de663e9c1108b
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-08-01 19:59:43 +0700

    Update blogs.js

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,4 +1,5 @@
-const Blog = require('../models/blog')
+const Blog = require('../models/blog.model')
+const User = require('../models/user.model')
 const router = require('express').Router()
 
 router.get('/', async (req, res) => {
@@ -11,10 +12,21 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  const blog = new Blog(req.body)
+  const body = req.body
+
+  const user = await User.findById(body.userId)
 
   try {
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      user: user._id
+    })
+
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     res.json(savedBlog)
   } catch (e) {
     res.status(400).json(e)
