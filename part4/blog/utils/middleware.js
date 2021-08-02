b37fe823c6e25fbda7e9f616777d17abb8cf133a
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-08-02 20:05:23 +0700

    Update utils

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -1,31 +1,60 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
-const reqLogger = (req, res, next) => {
-  logger.info('Method: ', req.method)
-  logger.info('Path: ', req.path)
-  logger.info('Body: ', req.body)
+const requestLogger = (request, response, next) => {
+  logger.info('Method: ', request.method)
+  logger.info('Path: ', request.path)
+  logger.info('Body: ', request.body)
   logger.info('---')
   next()
 }
 
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
-const errorHandler = (err, req, res, next) => {
-  logger.error(err.message)
-
-  if (err.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
-  } else if (err.name === 'ValidationError') {
-    return res.status(400).json({ error: err.message })
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'Casterror') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'Validationerror') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenerror') {
+    return response.status(401).json({ error: 'invalid token' })
   }
 
-  next(err)
+  logger.error(error.message)
+  next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+  const auth = request.get('authorization')
+  if (auth && auth.toLowerCase().startsWith('bearer ')) {
+    request.token = auth.substring(7)
+  } else {
+    request.token = null
+  }
+
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    request.decodedToken = decodedToken
+  } catch (error) {
+    request.decodedToken = null
+  }
+
+  next()
+}
+
+const userExtractor = (request, response, next) => {
+  request.user = request.body.userid
+
+  next()
 }
 
 module.exports = {
-  reqLogger,
+  requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor,
+  userExtractor
 }
