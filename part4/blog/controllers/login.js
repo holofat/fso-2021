e713137ed30bc3719496d10f84aefd36ba78923e
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-08-02 20:05:09 +0700

    Create login.js

---
+++ part4/blog/controllers/login.js
@@ -0,0 +1,36 @@
+const router = require('express').Router()
+const User = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+router.post('/', async (req, res) => {
+  const body = req.body
+  console.log(body)
+
+  try {
+    const user = await User.findOne({ username: body.username })
+
+    const passwordCorrect = user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash)
+
+    if (!(user && passwordCorrect)) {
+      return res.status(401).json({
+        error: 'invalid username or password'
+      })
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET)
+
+    res.status(200).send({ token, username: user.username, name: user.name })
+  } catch (e) {
+    res.json(e)
+  }
+})
+
+module.exports = router
