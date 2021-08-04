809f308591f1ebdf8b5bd28177f33f7bba3c4343
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2021-08-04 15:16:32 +0700

    Update login feature

--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({ handleUsername, handlePassword, username, password, handleLogin }) => (
   <div>
@@ -13,5 +14,13 @@ const LoginForm = ({ handleUsername, handlePassword, username, password, handleL
     </form>
   </div>
 )
+
+LoginForm.propTypes = {
+  handleLogin: PropTypes.func.isRequired,
+  handleUsername: PropTypes.func.isRequired,
+  handlePassword: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
+}
 
 export default LoginForm
